404b52fc0bea1e64ad6b5fff7adc0e187e1549d5
Malik <malik.krijestorac@stu.ssst.edu.ba>
2023-04-09 23:30:31 +0200

    Added end-points in FeedbackController

--- teach.me/src/main/java/com/teach/me/controllers/FeedbackController.java
+++ teach.me/src/main/java/com/teach/me/controllers/FeedbackController.java
@@ -1,5 +1,27 @@
 package com.teach.me.controllers;
 
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("/feedback")  // ../feedback --> dolazi se do kontrolera
+@RestController()
 public class FeedbackController {
-    
+
+
+
+    @PostMapping()
+    public void createFeedback(){
+
+    }
+
+    @PutMapping("/id")
+    public void updateFeedback(long feedbackId){
+
+    }
+
+    @DeleteMapping()
+    public void deleteFeedback(long feedbackId){
+
+    }
+
+
 }
--- teach.me/src/main/java/com/teach/me/controllers/ProfessorController.java
+++ teach.me/src/main/java/com/teach/me/controllers/ProfessorController.java
@@ -1,4 +1,8 @@
 package com.teach.me.controllers;
 
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ProfessorController {
 }
