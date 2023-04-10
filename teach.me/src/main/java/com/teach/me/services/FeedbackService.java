4aba334e6e61508279462ea67455d32327d3c625
Malik <malik.krijestorac@stu.ssst.edu.ba>
2023-04-10 23:41:39 +0200

    Implemented FeedbackService with appropriate methods

--- teach.me/src/main/java/com/teach/me/services/FeedbackService.java
+++ teach.me/src/main/java/com/teach/me/services/FeedbackService.java
@@ -1,4 +1,30 @@
 package com.teach.me.services;
 
+import com.teach.me.models.FeedbackDto;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
 public class FeedbackService {
+
+    public FeedbackDto getFeedback(long id) {
+        return new FeedbackDto(id, "Complaint", "Math350", "Zeljko Juric", new Date(), "Everything ok");
+    }
+
+    public FeedbackDto createFeedback(FeedbackDto feedback) {
+        feedback.setId(156L);
+        feedback.setDate(new Date());
+        // todo set.professorId() here or just put a name?
+        return feedback;
+    }
+
+    public FeedbackDto updateFeedback(long id, FeedbackDto feedback) {
+        feedback.setId(id);
+        return feedback;
+    }
+
+    public void deleteFeedback(long id) {
+        System.out.println("Deleted feedback with id" + id);
+    }
 }
