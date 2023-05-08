691ccb9449c9ffe3dc6ecf6dee1ed6abf3bc36f3
Kenan Jahic <kenanjahicc@gmail.com>
2023-05-09 00:18:33 +0200

    heroku config

--- teach.me/src/main/java/com/teach/me/config/CorsConfiguration.java
+++ teach.me/src/main/java/com/teach/me/config/CorsConfiguration.java
@@ -1,5 +1,5 @@
 package com.teach.me.config;
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -7,7 +7,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfiguration {
-
+    @Value("${frontend.url}")
+    private String[] frontendUrl;
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
@@ -15,7 +16,7 @@ public class CorsConfiguration {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedMethods("*")
-                        .allowedOrigins("http://localhost:4200/");
+                        .allowedOrigins(frontendUrl);
             }
         };
     }
