98ee4ec2f6620d0cb97cf6054cd05becfd50aeb8
Malik <malik.krijestorac@stu.ssst.edu.ba>
2023-05-15 22:04:17 +0200

    Implemented tests for professor and feedback services

--- src/main/java/com/teach/me/services/ProfessorService.java
+++ src/main/java/com/teach/me/services/ProfessorService.java
@@ -5,7 +5,7 @@ import com.teach.me.models.entities.Professor;
 import com.teach.me.repositories.ProfessorRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
+import java.util.Optional;
 
 @Service
 public class ProfessorService {
@@ -17,7 +17,15 @@ public class ProfessorService {
     }
 
     public ProfessorDto getProfessor(long id) {
-        Professor professor = professorRepository.getById(id);
+        Professor professor = getEntity(id);
         return new ProfessorDto(professor.getId(), professor.getFullName(), professor.getImageUrl(), professor.getCourses(), professor.getCvUrl(), professor.getHobbies(), professor.getRating());
+    }
+
+    public Professor getEntity(long id) {
+        Optional<Professor> professorOptional = professorRepository.findById(id);
+        if (professorOptional.isPresent()) {
+            return professorOptional.get();
+        }
+        throw new RuntimeException("does not exist");
     }
 }
---
+++ src/test/java/com/teach/me/services/FeedbackServiceUnitTest.java
@@ -0,0 +1,133 @@
+package com.teach.me.services;
+
+import com.teach.me.data.FeedbackTest;
+import com.teach.me.models.FeedbackDto;
+import com.teach.me.models.entities.Feedback;
+import com.teach.me.repositories.FeedbackRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@RunWith(SpringRunner.class)
+public class FeedbackServiceUnitTest {
+
+    @MockBean
+    private FeedbackRepository feedbackRepository;
+
+    @TestConfiguration
+    static class FeedbackServiceTestConfiguration {
+
+        @Bean
+        @Primary
+        public FeedbackService FeedbackService(FeedbackRepository feedbackRepository) {
+            return new FeedbackService(feedbackRepository);
+        }
+    }
+
+    @Autowired
+    private FeedbackService feedbackService;
+
+    @Test
+    public void givenValidId_whenGetFeedback_thenFeedbackShouldBeFound() {
+        long id = 1L;
+        Mockito.when(feedbackRepository.findById(id))
+                .thenReturn(Optional.of(FeedbackTest.feedback()));
+
+        FeedbackDto resultFeedback = feedbackService.getFeedback(id);
+
+        assertThat(resultFeedback.getTitle()).isEqualTo("Complaint");
+    }
+
+    @Test
+    public void givenInvalidId_whenGetFeedback_thenExceptionShouldBeThrown() {
+        assertThatThrownBy(() -> feedbackService.getFeedback(3L))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessageContaining("does not exist");
+    }
+
+    @Test
+    public void givenMilestone_whenCreateMilestone_thenMilestoneIsReturned() {
+
+        FeedbackDto inputFeedbackDto = FeedbackTest.feedbackDto1();
+        inputFeedbackDto.setId(0L);
+        Feedback outputFeedback = FeedbackTest.feedback();
+
+        Mockito.when(feedbackRepository.save(any(Feedback.class)))
+                .thenReturn(outputFeedback);
+
+
+        FeedbackDto resultFeedback = new FeedbackService(feedbackRepository).createFeedback(inputFeedbackDto); //???
+
+
+        assertThat(resultFeedback).isNotNull();
+        assertThat(resultFeedback.getTitle()).isEqualTo(inputFeedbackDto.getTitle());
+        assertThat(resultFeedback.getId()).isNotEqualTo(2L);
+    }
+
+    @Test
+    public void givenFeedback_whenCreateFeedback_thenRepositoryCalled() {
+
+        FeedbackDto feedbackDto = FeedbackTest.feedbackDto3();
+
+        Mockito.when(feedbackRepository.save(any(Feedback.class)))
+                .thenReturn(FeedbackTest.feedback());
+
+        feedbackService.createFeedback(feedbackDto);
+
+        verify(feedbackRepository, times(1)).save(any(Feedback.class));
+    }
+
+    @Test
+    public void givenMilestoneAndValidId_whenUpdate_thenMilestoneReturned() {
+        // arrange
+        FeedbackDto inputFeedbackDto = FeedbackTest.feedbackDto1();
+        inputFeedbackDto.setId(1L); // reset id
+        long id = 1L;
+        Feedback outputFeedback = FeedbackTest.feedback();
+        outputFeedback.setId(id);
+
+        Mockito.when(feedbackRepository.findById(id))
+                .thenReturn(Optional.of(outputFeedback));
+        Mockito.when(feedbackRepository.save(any(Feedback.class)))
+                .thenReturn(outputFeedback);
+
+        // act
+        FeedbackDto resultFeedback = feedbackService.updateFeedback(id, inputFeedbackDto);
+
+        // assert
+        assertThat(resultFeedback).isNotNull();
+        assertThat(resultFeedback.getTitle()).isEqualTo(inputFeedbackDto.getTitle());
+        assertThat(resultFeedback.getId()).isEqualTo(id);
+    }
+
+    @Test
+    public void givenInvalidId_whenUpdate_thenExceptionShouldBeThrown() {
+        assertThatThrownBy(() -> feedbackService.updateFeedback(2L, FeedbackTest.feedbackDto3()))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessageContaining("does not exist");
+    }
+
+    @Test
+    public void givenFeeback_whenDelete_thenRepositoryCalled() {
+
+        long id = 1L;
+
+        feedbackService.deleteFeedback(id);
+
+        verify(feedbackRepository, times(1)).deleteById(id);
+    }
+}
